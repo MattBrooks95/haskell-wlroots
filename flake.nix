e9943730e5106cfbadc32cb627da955632dec7b4
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2023-12-30 22:46:32 +0900

    Squashed commit of the following:
    commit d8e3edcc65554424a39db36b62c61c00ac1648ff
    Author: MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
    Date:   Sat Dec 30 20:09:24 2023 +0900
    
        grab wayland and wlroots

--- flake.nix
+++ flake.nix
@@ -11,8 +11,6 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
-        # need to match Stackage LTS version
-        # from stack.yaml resolver
         hPkgs = pkgs.haskell.packages."ghc96";
 
         myDevTools = [
@@ -23,14 +21,25 @@
           hPkgs.implicit-hie # auto generate LSP hie.yaml file from cabal
           pkgs.zlib # External C library needed by some Haskell packages
         ];
+        externalLibraries = [
+          pkgs.wlroots
+          pkgs.wayland
+        ];
+        libraryPathItems = myDevTools ++ externalLibraries;
       in {
         devShells.default = pkgs.mkShell {
-          buildInputs = myDevTools;
+          buildInputs = libraryPathItems;
+          nativeBuildInputs = [
+            # necessary so wlrroots can find libudev
+            pkgs.pkg-config
+            # necessary so wlrroots can find libudev
+            pkgs.systemd
+          ];
 
           # Make external Nix c libraries like zlib known to GHC, like
           # pkgs.haskell.lib.buildStackProject does
           # https://github.com/NixOS/nixpkgs/blob/d64780ea0e22b5f61cd6012a456869c702a72f20/pkgs/development/haskell-modules/generic-stack-builder.nix#L38
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath myDevTools;
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libraryPathItems;
         };
       }
     );
